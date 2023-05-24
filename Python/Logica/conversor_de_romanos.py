c2dbf13140185908f3eca32d4c3109ae31962b70
Guilherme freitas <130511591+freitas3s@users.noreply.github.com>
2023-05-24 20:53:51 -0300

    ...

---
+++ Python/Logica/conversor_de_romanos.py
@@ -0,0 +1,21 @@
+romanos= {
+ 'I': 1,
+ 'V': 5,
+ 'X': 10,
+ 'L': 50,
+ 'C': 100,
+ 'D': 500,
+ 'M': 1000,
+}
+
+def conversor( romano ):
+    numero=0
+    for algarismo in romano[::-1]:
+        if romanos[algarismo]< numero:
+            numero-=romanos[algarismo]
+        else:
+            numero+=romanos[algarismo]
+
+    print(f"este numero é: {numero}")
+
+conversor("iii".upper())
