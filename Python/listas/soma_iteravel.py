7bb1fc38fbb10b75df030b9ded3ae77427cc5cbb
Guilherme freitas <130511591+freitas3s@users.noreply.github.com>
2023-05-26 22:05:46 -0300

    .

---
+++ Python/listas/soma_iteravel.py
@@ -0,0 +1,34 @@
+def soma(a,b):
+    lista1=[]
+    lista2=[]
+    resultado=[]
+    for i in str(a) :
+        lista1.append(i)
+    lista1.reverse()
+
+    while len(lista2)<len(lista1):
+        lista2.append(str(0))
+
+    for i in str(b):
+        lista2.append(i)
+    lista2.reverse()
+
+
+    resto=0
+    for i in range(len(lista1)):
+        soma=int(lista1[i])+int(lista2[i])+int(resto)
+
+        if soma>=10:
+            diferença=soma-10
+            resto=1
+            resultado.append(diferença)
+        else:
+            resto=0
+            resultado.append(soma)
+
+
+    resultado.reverse()
+    return resultado
+
+
+print(soma(150,255))
