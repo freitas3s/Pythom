f22407e046dd872195ea56b04a4e70e7bde45ea5
Guilherme freitas <130511591+freitas3s@users.noreply.github.com>
2023-05-27 22:57:59 -0300

    ....

--- Python/if/execicio_crime.py
+++ Python/if/execicio_crime.py
@@ -2,38 +2,34 @@ print("Dê o seu depoimento e contribua para a investigação")
 print("")
 
 telefonou = input("Ligou para vitima?")
-if telefonou == "sim":
-    telefonou = int(1)
-else: 
-    telefonou = int(0)
+
+def somador(resposta):
+    
+    if resposta== "sim":
+        return 1
+    else: 
+        return 0
+
+contador=somador(telefonou)
+
 local = input("Esteve no local do crime?")
-if local == "sim":
-    local = int(1)
-else:
-    local = int(0)
+contador+=somador(local)
 onde_mora = input("Mora proximo a vitima?")
-if onde_mora == "sim":
-    onde_mora= int(1)
-else:
-    onde_mora = int(0)
- 
+contador+=somador(onde_mora)
 devia = input("Devia para a vitima?")
-if devia == "sim":
-    devia= int(1)
-else:
-    devia = int(0)
+contador+=somador(devia)
 
 trabalhou = input("Já trabalhou com a vítima?")
-if trabalhou == "sim":
-    trabalhou= int(1)
-else:
-    trabalhou = int(0)
+contador+=somador(trabalhou)
 
-depoimento = telefonou + local + onde_mora + devia + trabalhou
+depoimento = contador
+
 if depoimento < 2 :
     print("Está liberado!")
+
 if depoimento == 2 :
     print ("Você é suspeito!!!")
+
 if depoimento == 3 or depoimento == 4 :
     print("Você é provavelmente um cúmplice!!!")
 
