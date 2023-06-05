b37749dadd72f2a5aaaed05130a00c74ca9d8b4c
Guilherme freitas <130511591+freitas3s@users.noreply.github.com>
2023-06-05 13:24:26 -0300

    ...

---
+++ Python/aleatórios/apagador_de_emails.py
@@ -0,0 +1,10 @@
+import pyautogui as pg
+import time
+time.sleep(10)
+#selecionar=(x=413, y=186)
+#naolidas=(x=434, y=315)
+#lixeira=(x=541, y=184)
+while True:
+    pg.click(388,191,1)
+    pg.click(541,184,1)
+    time.sleep(3)
