46a3f671f46915fb52287a622d1561223756f9d2
Guilherme freitas <130511591+freitas3s@users.noreply.github.com>
2023-06-05 20:40:09 -0300

    .. .

--- Python/POO/classes.py
+++ Python/POO/classes.py
@@ -2,8 +2,30 @@
 #instancia da classe tem dados 
 #umas classe pode gerar varias instancias
 #na classe o self é a propria instancia
-
-class Animal:
-    def __init__(self, nome):
+import json
+import os 
+path=r'C:\Users\guije\Documents\GitHub\teste\Python\POO\pessoas.json'
+class pessoa:
+    def __init__(self, nome, idade):
         self.nome = nome
-        
+        self.idade=idade
+
+def cria_pessoa(nome,idade):
+    dados.append(pessoa(nome,idade))
+
+
+#salvando em JSON:
+dados = []
+
+while len(dados) <4:
+    nome=input('nome ')
+    idade=int(input('Idade '))
+    os.system("cls")
+    cria_pessoa(nome,idade)
+
+dados_formatados = [
+    vars(item) for item in dados
+]
+
+with open(path, 'w') as arquivo:
+    json.dump(dados_formatados,arquivo,ensure_ascii=False,indent=2)
